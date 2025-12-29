0a1b9b869be956faa0a53ff2d86ac9c0636baa75
Shaurya Luthra <shauryaluthra07@gmail.com>
2025-12-29 17:57:05 -0500

    Update App to use AI version and improve TicTacToeAI

--- App.java
+++ App.java
@@ -1,5 +1,24 @@
+import javax.swing.JOptionPane;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        TicTacToe tictactoe = new TicTacToe();
+    public static void main(String[] args) {
+
+        String[] options = {"2 Player", "Play vs AI"};
+        int choice = JOptionPane.showOptionDialog(
+                null,
+                "Choose game mode",
+                "Tic Tac Toe",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.INFORMATION_MESSAGE,
+                null,
+                options,
+                options[0]
+        );
+
+        if (choice == 0) {
+            new TicTacToe();
+        } else if (choice == 1) {
+            new TicTacToeAI();
+        }
     }
 }
